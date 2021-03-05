dd92679aac0e58f7ca8eaef592dd1e95afdbff40
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-03-05 10:53:46 +0800

    Added streambuilder for messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -11,7 +11,6 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   final _firestore = FirebaseFirestore.instance;
 
   final _auth = FirebaseAuth.instance;
@@ -52,13 +51,14 @@ class _ChatScreenState extends State<ChatScreen> {
 
   //Notifies firebase of a new message
   void messagesStream() async {
-      // Listens to all of the changes of the 'messages' collection
-      await for (var messageStream in _firestore.collection('messages').snapshots()) {
-        for (var message in messageStream.docs) {
-          print(message.data());
-        }
+    // Listens to all of the changes of the 'messages' collection
+    await for (var messageStream
+        in _firestore.collection('messages').snapshots()) {
+      for (var message in messageStream.docs) {
+        print(message.data());
       }
-      //Developer will be notified of any new changes in the collection
+    }
+    //Developer will be notified of any new changes in the collection
   }
 
   @override
@@ -83,6 +83,39 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            //Query Snapshot comes from cloud firestore
+            StreamBuilder<QuerySnapshot>(
+              //Where the streams will be coming from
+              stream: _firestore.collection('messages').snapshots(),
+              //Rebuilds the widget with the streams
+              //defined in docs -> context, snapshot -> different from firebase snapshot
+              builder: (context, snapshot) {
+                //builds a text widget
+                List<Text> messageWidgets = [];
+
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlue,
+                    ),
+                  );
+    }
+                  final messages = snapshot.data.docs;
+
+                  for (var message in messages) {
+                    final messageText = message.data()['text'];
+                    final messageSender = message.data()['sender'];
+
+                    final messageWidget =
+                        Text('$messageText from $messageSender\n');
+
+                    messageWidgets.add(messageWidget);
+                  }
+                  return Column(
+                    children: messageWidgets,
+                  );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -90,10 +123,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20
-                      ),
+                      style: TextStyle(color: Colors.black, fontSize: 20),
                       onChanged: (value) {
                         //Do something with the user input.
                         messageText = value;
@@ -104,13 +134,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   TextButton(
                     onPressed: () {
                       //messages is the document name in firestore
-                        _firestore.collection('messages').add({
-                          //text and sender are the types in firestore
-                          'text': messageText,
-                          'sender': loggedInUser.email,
-                        });
+                      _firestore.collection('messages').add({
+                        //text and sender are the types in firestore
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                      });
 
-                        messagesStream();
+                      messagesStream();
                     },
                     child: Text(
                       'Send',
